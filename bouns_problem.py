b5c4b50f2c10e1ebe92a149539a1b69fe12a136a
mhmdwageh <162148672+mhmdwageh@users.noreply.github.com>
2024-12-31 21:45:14 +0200

    Update bouns_problem.py

--- bouns_problem.py
+++ bouns_problem.py
@@ -1,6 +1,7 @@
 def isCanDistribute(a,b,c,n):
-    coinsPerOne = a+b+c+n
-    if (coinsPerOne) %3 ==0 and  coinsPerOne >= max(a,b,c):
+    totalcoins = a+b+c+n
+    coinsPerOne = totalcoins / 3
+    if (totalcoins) %3 == 0 and  coinsPerOne >= max(a,b,c):
             return "YES"
     else:
         return "NO"
@@ -10,4 +11,4 @@ t = int(input())
 while(t):
     a,b,c,n = map(int, input().split())
     print(isCanDistribute(a,b,c,n))
-    t = t-1
+    t -=1
